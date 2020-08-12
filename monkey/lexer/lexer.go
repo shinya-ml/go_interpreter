8f105f36f9220fdd5b7b3344c3c9ebf3b7db5cb1
shinya-ml <epsonia_ss08@icloud.com>
2020-08-12 11:19:44 +0900

    lexer_testの作成, lexerを作成するためのヘルパーメソッド作成

---
+++ monkey/lexer/lexer.go
@@ -0,0 +1,30 @@
+package lexer
+
+import "monkey/token"
+
+type Lexer struct {
+	input        string
+	position     int
+	readPosition int
+	ch           byte
+}
+
+func New(input string) *Lexer {
+	l := &Lexer{input: input}
+	l.readChar()
+	return l
+}
+
+func (l *Lexer) readChar() {
+	if l.readPosition >= len(l.input) {
+		l.ch = 0
+	} else {
+		l.ch = l.input[l.readPosition]
+	}
+	l.position = l.readPosition
+	l.readPosition += 1
+}
+
+func (l *Lexer) NextToken() token.Token {
+
+}
